dbe7ce36f224d07994cdec7de7bd5e2731a9ba57
denismir <d.smirnov@qkodklan.ru>
2021-11-26 00:16:08 +0400

    default data without details, models

--- nlg-parsers/processing/utils.py
+++ nlg-parsers/processing/utils.py
@@ -2,10 +2,13 @@ import requests
 import json
 from time import sleep
 from random import randint
-# from torrequest import TorRequest
-# from multiprocessing.pool import ThreadPool
-# from torpy.http.requests import tor_requests_session
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+import django
+django.setup()
 from requests_tor import RequestsTor
+from processing.models import Organization
+import uuid
 
 def simple_get_data():
     for i in range(1, 100):
@@ -19,37 +22,59 @@ def simple_get_data():
         sleep(randint(6, 15))
 
 
-# def get_data_with_tor(region_number):
-#     for i in range(1, 100):
-#         with TorRequest() as tr:
-#             response = tr.get('https://pb.nalog.ru/search-proc.json',
-#                               password='1234',
-#                               data={'mode': 'search-ul',
-#                                     'regionUl': region_number,
-#                                     'page': i,
-#                                     'pageSize': '100'})
-#             print(response.text)
-#             tr.reset_identity()
+with open('rerion_list.txt', mode='r') as f:
+    region_list = f.read().split(',')
 
-# get_data_with_tor('03')
-# with tor_requests_session() as s:  # returns requests.Session() object
-#     links = ['https://pb.nalog.ru/search-proc.json']
-#
-#     with ThreadPool(3) as pool:
-#         pool.map(s.get, links)
-#         print(s.text)
-# #
-# rt = RequestsTor()
-# rt.check_ip()
 
-# r = rt.post('https://pb.nalog.ru/search-proc.json', data={'mode': 'search-ul',
-#                                                                         'regionUl': '03',
-#                                                                         'page': '250',
-#                                                                         'pageSize': '100'})
-# print(r.text)
+rt = RequestsTor(tor_ports=(9050,), tor_cport=9051, password='password')
+regionUl = '20'
+url = 'https://pb.nalog.ru/search-proc.json'
+flag = True
 
-rt = RequestsTor(tor_ports=(9050,), tor_cport=9051, autochange_id=1)
-url = 'https://httpbin.org/anything'
-r = rt.get(url)
-rt.test()
-print(r.text)
+def get_company_details(token):
+    url = 'https://pb.nalog.ru/company-proc.json'
+    data = {'method': 'get-response',
+            'token': token}
+    print(data)
+    r = rt.post(url=url, data=data)
+    print(r.text)
+    print('THE R: ', r)
+    j_company_details = json.loads(r.text)
+    print('=====')
+    print('j_company_details ', j_company_details)
+    return j_company_details
+
+region_list = ['77']
+
+for zone in region_list:
+    page = 1
+    while flag:
+        data = {'mode': 'search-ul',
+                'regionUl': zone,
+                'page': page,
+                'pageSize': '100'}
+        r = rt.post(url=url, data=data)
+        print(r.text)
+        jr = json.loads(r.text)
+        print('JR DATA: ', jr)
+        for item in jr['ul']['data']:
+            print('ITEM ', item, type(item))
+            company_details = get_company_details(item['token'])
+            try:
+                Organization.objects.create(yearcode=item['yearcode'],
+                                            periodcode=item['yearcode'],
+                                            inn=item['inn'],
+                                            ogrn=company_details['ОГРН'],
+                                            regionname=item['regionname'],
+                                            namep=item['namep'],
+                                            namec=item['namec'],
+                                            invalid=item['invalid'],
+                                            okved2=item['okved2'],
+                                            okved2name=item['okved2name'],
+                                            token=item['token'],
+                )
+            except Exception as e:
+                with open('error.log', mode='a') as f:
+                    f.write(e + '\n')
+        page += 1
+        flag = jr['ul']['hasMore']
